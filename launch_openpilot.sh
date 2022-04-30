52c060f9fd599e31eaa56fc05404ca7e5ce8d4f7
Thomas <thomas510111@gmail.com>
2022-04-30 22:22:22 +0300

    Use retropilot backend and restore upload raw https://github.com/zorrobyte/zorropilot/commit/8e3112a89b8df5948a72d3c52abfc02e3d481812.patch

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
 export PASSIVE="0"
+export API_HOST="https://api.retropilot.org"
 exec ./launch_chffrplus.sh
 
--- selfdrive/loggerd/tests/loggerd_tests_common.py
+++ selfdrive/loggerd/tests/loggerd_tests_common.py
@@ -56,6 +56,7 @@ class MockParams():
   def __init__(self):
     self.params = {
       "DongleId": b"0000000000000000",
+      "UploadRaw": b"1",
       "IsOffroad": b"1",
     }
 
