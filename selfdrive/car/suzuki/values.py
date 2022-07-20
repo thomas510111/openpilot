6be1bb3c8628bab428898271e4fcaa7a271ab1ae
Thomas <thomas510111@gmail.com>
2022-07-20 12:35:08 +0300

    Add speed to car state

--- selfdrive/car/suzuki/values.py
+++ selfdrive/car/suzuki/values.py
@@ -1,2 +1,10 @@
 class CAR:
   SUZUKI = 'suzuki'
+  
+class CANBUS:
+  pt = 0
+  cam = 2 
+  
+  
+class DBC_FILES:
+  suzuki_vitara = "suzuki_vitara"  # Used for all cars with MQB-style CAN messaging 
